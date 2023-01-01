9eeb49e14f2eade3253b6055b2258472e6c5273a
Saurabh Maurya <saurabhkumar444.saurabhkumar19682@gmail.com>
2023-01-01 13:31:01 +0530

    create function component and call function

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = (
+const Footer = () => {
+  return (
+    <div>
+      <h3>Parcel is beast</h3>
+    </div>
+  );
+};
+const header = (
   <div>
-    <h1 key="h1">hello</h1>
-    <h2 key="h2">World</h2>
+    <h1>Saurabh Here</h1>
   </div>
 );
+const Heading = () => {
+  return (
+    <div>
+      {header}
+      {console.log("Any JS code we can write here")}
+      <h1 key="h1">I'm a Software developer</h1>
+      <h2 key="h2">React is beautiful</h2>
+      {Footer()}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Heading);
+root.render(<Heading />);
