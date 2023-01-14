39353608f6b0e9f7b54db6daf39193a50eb3a07a
Saurabh Maurya <saurabhkumar444.saurabhkumar19682@gmail.com>
2023-01-14 20:47:32 +0530

    add header logo and input

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 const Footer = () => {
@@ -9,8 +9,41 @@ const Footer = () => {
   );
 };
 const header = (
-  <div>
-    <h1>Saurabh Here</h1>
+  <div
+    style={{
+      justifyContent: "space-between",
+      display: "flex",
+    }}
+  >
+    <img
+      src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+      alt="logo"
+      style={{ height: 40, width: 40 }}
+    />
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <input
+        onChange={(e, value) => {
+          console.log(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        Search
+      </button>
+    </div>
+    <img
+      src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars.png"
+      alt="logo"
+      style={{ height: 40, width: 40 }}
+    />
   </div>
 );
 const Heading = () => {
@@ -20,7 +53,7 @@ const Heading = () => {
       {console.log("Any JS code we can write here")}
       <h1 key="h1">I'm a Software developer</h1>
       <h2 key="h2">React is beautiful</h2>
-      {Footer()}
+      <Footer />
     </div>
   );
 };
